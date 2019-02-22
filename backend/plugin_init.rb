84dd67e1d2af2c33d6285d3c7dbad496eef9dba3
James Bullen <jambun@gmail.com>
2019-02-22 22:02:39 +1100

    add permission to approve

---
+++ backend/plugin_init.rb
@@ -0,0 +1,9 @@
+Permission.define("manage_agency_registration",
+                  "The ability to manage the agency registration workflow",
+                  :level => "repository")
+
+Permission.define("approve_agency_registration",
+                  "The ability to approve the registration of a draft agency",
+                  :implied_by => "manage_agency_creation",
+                  :level => "global")
+
