0c221328a721477aeacaeebb34880c6bb4c625cd
James Bullen <jambun@gmail.com>
2019-02-26 13:22:35 +1100

    add registration_last_action to schema

--- schemas/agent_corporate_entity_ext.rb
+++ schemas/agent_corporate_entity_ext.rb
@@ -4,6 +4,10 @@
     "enum" => ["draft", "submitted", "approved"],
     "readonly" => true
   },
+  "registration_last_action" => {
+    "type" => "string",
+    "readonly" => true
+  },
   "registration_last_user" => {
     "type" => "string",
     "readonly" => true
