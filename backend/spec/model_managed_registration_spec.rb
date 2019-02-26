2fb81fb4f5ea5f9b5d7c11c5666ac9d0030f387d
James Bullen <jambun@gmail.com>
2019-02-27 09:42:37 +1100

    more backend tests. slightly broken

--- backend/spec/model_managed_registration_spec.rb
+++ backend/spec/model_managed_registration_spec.rb
@@ -48,6 +48,45 @@ describe 'Managed Registration' do
 
       expect{ agency.update_from_json(JSONModel(:agent_corporate_entity).find(agency.id)) }.to raise_error(ReadOnlyException)
     end
+
+
+    it 'cannot be published while in draft' do
+      json = JSONModel(:agent_corporate_entity).find(agency.id)
+      json['publish'] = true
+      agency.update_from_json(json)
+      json = JSONModel(:agent_corporate_entity).find(agency.id)
+      expect(json['publish']).to be_falsey
+    end
+
+
+    it 'forces publish and registration_state on create' do
+      forced_agency = AgentCorporateEntity.create_from_json(build(:json_agent_corporate_entity,
+                                                                  {:publish => true, :registration_state => 'approved'}))
+
+      expect(forced_agency.publish).to eq(0)
+      expect(forced_agency.registration_state).to eq('draft')
+    end
+
+
+    it 'gets a handy dandy label to indicate an unapproved state' do
+      json = JSONModel(:agent_corporate_entity).find(agency.id)
+      expect(json['title']).to match(/^\[DRAFT\] /)
+
+      Registration.submit(agency)
+
+      json = JSONModel(:agent_corporate_entity).find(agency.id)
+      expect(json['title']).to match(/^\[SUBMITTED\] /)
+    end
+
+
+    it 'can only be submitted by a user who can edit agents' do
+      create(:user, {:username => 'noperms'})
+
+      as_test_user('noperms') do
+        Registration.submit(agency)
+#        expect(Registration.submit(agency)).to raise_error(ReadOnlyException)
+      end
+    end
   end
 
 end
