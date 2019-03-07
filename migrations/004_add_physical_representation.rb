e711c5f5f31db5bc6890cde5544fea6c17f4e758
Mark Triggs <mark@dishevelled.net>
2019-03-07 16:48:33 +1100

    Add enum values from spreadsheet & translate them

--- migrations/004_add_physical_representation.rb
+++ migrations/004_add_physical_representation.rb
@@ -40,15 +40,59 @@ Sequel.migration do
       add_foreign_key([:resource_id], :resource, :key => :id)
     end
 
-    create_enum('runcorn_access_category', ['FIXME'])
-    create_enum('runcorn_location', ['FIXME'])
-    create_enum('runcorn_access_clearance_procedure', ['FIXME'])
-    create_enum('runcorn_accessioned_status', ['FIXME'])
-    create_enum('runcorn_colour', ['FIXME'])
-    create_enum('runcorn_format', ['FIXME'])
+    create_enum('runcorn_access_category', [
+                  '000', '001', '002', '003', '004', '005', '006', '007', '008', '009', '010',
+                  '011', '012', '013', '014', '015', '016', '017', '018', '019', '020', '021',
+                  '022', '023', '024', '025', '026', '027', '028', '029', '030', '031', '032',
+                  '033', '034', '035', '036', '037', '038', '039', '040', '041', '042', '043',
+                  '044', '045', '046', '047', '048', '049', '050', '051', '052', '053', '054',
+                  '055', '056', '057', '058', '059', '060', '061', '062', '063', '064', '065',
+                  '066', '067', '068', '069', '070', '071', '072', '073', '074', '075', '076',
+                  '077', '078', '079', '080', '081', '082', '083', '084', '085', '086', '087',
+                  '088', '089', '090', '091', '092', '093', '094', '095', '096', '097', '098',
+                  '099', '100', '101', '102', '999', 'CON', 'DEA', 'DES', 'MAS', 'NAP', 'OVR',
+                  'PRA', 'REF', 'TEM',
+                ])
+
+    create_enum('runcorn_location',
+                ['ATT', 'CAM', 'COND', 'CONS', 'CPH', 'DIG', 'DISC', 'EXH', 'EXTWEB', 'FIL',
+                 'FVT', 'JOL', 'MIC', 'MSS', 'N/A', 'OUT', 'PER', 'PSR', 'REF', 'REF SCAN', 'REP',
+                 'REP2', 'REPRO', 'SCAN PART', 'SCAN RR', 'SCANNED', 'SCANNED EX', 'SEE CON', 'SORTRM',
+                 'VAULT1', 'VAULT2'])
+
+
+    create_enum('runcorn_access_clearance_procedure',
+                ['ADMIN',
+                 'BLANK',
+                 'OPEN',
+                 'RTI',
+                ])
+
+    create_enum('runcorn_accessioned_status',
+                ['ACCSSN',
+                 'DEACC',
+                 'DESTR',
+                 'PEND_DEACC',
+                 'PEND_DESTR',
+                ])
+
+    create_enum('runcorn_colour',
+                ['COL',
+                 'MON',
+                ])
+
+    create_enum('runcorn_format',
+                ['A/V', 'AER', 'ARC', 'ART', 'CAR', 'COM',
+                 'FCH', 'FIL', 'FLM', 'GRA', 'LFFLM', 'MAP', 'MIC', 'MMF', 'MSC',
+                 'NEG', 'OBJ', 'OTH', 'PAS', 'PHO', 'TEC', 'VOL', ])
+
+    create_enum('runcorn_salvage_priority_code',
+                ['AIC', 'BHG', 'LOW'])
+
+
     create_enum('runcorn_physical_description_type', ['FIXME'])
     create_enum('runcorn_physical_preservation_restriction_status', ['FIXME'])
-    create_enum('runcorn_salvage_priority_code', ['FIXME'])
+
 
     create_table(:representation_approved_by_rlshp) do
       primary_key :id
