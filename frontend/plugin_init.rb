3e1197508f81830d5604822545635f3d4c090a35
Payten Giles <payten.giles@gmail.com>
2019-03-07 12:49:24 +1100

    Frontend bits

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,1 +1,15 @@
 ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
+
+Rails.application.config.after_initialize do
+  Plugins.register_plugin_section(
+    Plugins::PluginSubRecord.new(
+      'series_system',
+      'physical_representation',
+      ['resource', 'archival_object'],
+      {
+        template_name: 'physical_representation',
+        heading_text:  I18n.t('physical_representation._singular'),
+      }
+    )
+  )
+end
