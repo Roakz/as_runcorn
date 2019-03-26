464f6d6d4a4bc79bda2b24ba12c30391d9a239bb
Payten Giles <payten.giles@gmail.com>
2019-03-27 10:16:43 +1100

    Don't need FactoryBotSyntaxHelpers in modify_factories_spec.rb

--- backend/spec/modify_factories_spec.rb
+++ backend/spec/modify_factories_spec.rb
@@ -1,12 +1,10 @@
-include FactoryBotSyntaxHelpers
-
 # Override json_date now that 'approximate' is the only valid option
 FactoryBot.modify do
-  factory :json_date, class: JSONModel(:date) do
+  factory :json_date, class: JSONModel::JSONModel(:date) do
     self.certainty 'approximate'
   end
 
-  factory :json_date_single, class: JSONModel(:date) do
+  factory :json_date_single, class: JSONModel::JSONModel(:date) do
     self.certainty 'approximate'
   end
 end
