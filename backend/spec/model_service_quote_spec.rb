41a4b13f36ceaad7ecc77f58629f7b794c620576
James Bullen <jambun@gmail.com>
2019-05-20 10:42:30 +1000

    Tidied up test

--- backend/spec/model_service_quote_spec.rb
+++ backend/spec/model_service_quote_spec.rb
@@ -3,57 +3,63 @@ require 'spec_helper'
 describe 'Runcorn Charges' do
   describe 'ServiceQuote model' do
 
+    let(:call_out) do
+      ChargeableItem.create_from_json(build(:json_chargeable_item,
+                                            :price_cents => 5000,
+                                            :description => 'You call, we come',
+                                            :charge_quantity_unit => 'order'))
+    end
+
+    let(:chlorine) do
+      ChargeableItem.create_from_json(build(:json_chargeable_item,
+                                            :price_cents => 1995,
+                                            :description => 'Kill the bugs',
+                                            :charge_quantity_unit => 'record'))
+    end
+
+    let(:work) do
+      ChargeableItem.create_from_json(build(:json_chargeable_item,
+                                            :price_cents => 300,
+                                            :description => 'Minimum wage mofos',
+                                            :charge_quantity_unit => 'qtr_hour'))
+    end
+
+    let(:pool_cleaning) do
+      ChargeableService.create_from_json(build(:json_chargeable_service,
+                                               :name => 'Pool Cleaning Service',
+                                               :description => 'We clean your pool!',
+                                               :service_items => [{'ref' => call_out.uri},
+                                                                  {'ref' => chlorine.uri},
+                                                                  {'ref' => work.uri}]))
+    end
+
+
     it 'can calculate a total charge' do
-      call_out = ChargeableItem.create_from_json(build(:json_chargeable_item,
-                                                       :price_cents => 5000,
-                                                       :description => 'You call, we come',
-                                                       :charge_quantity_unit => 'order'))
-
-      chlorine = ChargeableItem.create_from_json(build(:json_chargeable_item,
-                                                       :price_cents => 1995,
-                                                       :description => 'Kill the bugs',
-                                                       :charge_quantity_unit => 'record'))
-
-      work = ChargeableItem.create_from_json(build(:json_chargeable_item,
-                                                   :price_cents => 300,
-                                                   :description => 'Minimum wage mofos',
-                                                   :charge_quantity_unit => 'qtr_hour'))
 
       items = [
-               {'ref' => call_out.uri},
-               {'ref' => chlorine.uri},
-               {'ref' => work.uri},
+               {
+                 'quantity' => 1,
+                 'chargeable_item' => {
+                   'ref' => call_out.uri
+                 }
+               },
+               {
+                 'quantity' => 3,
+                 'chargeable_item' => {
+                   'ref' => chlorine.uri
+                 }
+               },
+               {
+                 'quantity' => 8,
+                 'chargeable_item' => {
+                   'ref' => work.uri
+                 }
+               }
               ]
-
-      pool_cleaning = ChargeableService.create_from_json(build(:json_chargeable_service,
-                                                               :name => 'Pool Cleaning Service',
-                                                               :description => 'We clean your pool!',
-                                                               :service_items => items))
-
 
       we_cleaned_your_pool = ServiceQuote.create_from_json(build(:json_service_quote,
                                                                  :chargeable_service => {'ref' => pool_cleaning.uri},
-                                                                 :line_items => [
-                                                                                 {
-                                                                                   'quantity' => 1,
-                                                                                   'chargeable_item' => {
-                                                                                     'ref' => call_out.uri
-                                                                                   }
-                                                                                 },
-                                                                                 {
-                                                                                   'quantity' => 3,
-                                                                                   'chargeable_item' => {
-                                                                                     'ref' => chlorine.uri
-                                                                                   }
-                                                                                 },
-                                                                                 {
-                                                                                   'quantity' => 8,
-                                                                                   'chargeable_item' => {
-                                                                                     'ref' => work.uri
-                                                                                   }
-                                                                                 }
-                                                                                ]))
-
+                                                                 :line_items => items))
 
       json = URIResolver.resolve_references(ServiceQuote.to_jsonmodel(we_cleaned_your_pool.id), ['chargeable_item'])
 
