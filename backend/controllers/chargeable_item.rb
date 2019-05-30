2cc99253f75fa17e72f085f72f47f2c895553135
James Bullen <jambun@gmail.com>
2019-05-30 10:00:49 +1000

    Backend endpoints for chargeable items and services

---
+++ backend/controllers/chargeable_item.rb
@@ -0,0 +1,47 @@
+class ArchivesSpaceService < Sinatra::Base
+  Endpoint.post('/chargeable_items')
+    .description("Create a Chargeable Item")
+    .params(["chargeable_item", JSONModel(:chargeable_item), "The updated record", :body => true])
+    .permissions([])
+    .returns([200, :created]) \
+  do
+    handle_create(ChargeableItem, params[:chargeable_item])
+  end
+
+  Endpoint.post('/chargeable_items/:id')
+    .description("Update a Chargeable Item")
+    .params(["id", :id],
+            ["chargeable_item", JSONModel(:chargeable_item), "The updated record", :body => true])
+    .permissions([])
+    .returns([200, :updated]) \
+  do
+    handle_update(ChargeableItem, params[:id], params[:chargeable_item])
+  end
+
+  Endpoint.delete('/chargeable_items/:id')
+    .description("Delete a Chargeable Item")
+    .params(["id", :id])
+    .permissions([])
+    .returns([200, :deleted]) \
+  do
+    handle_delete(ChargeableItem, params[:id])
+  end
+
+  Endpoint.get('/chargeable_items/:id')
+    .description("Get a Chargeable Item by ID")
+    .params(["id", :id])
+    .permissions([])
+    .returns([200, "(:chargeable_item)"]) \
+  do
+    json_response(ChargeableItem.to_jsonmodel(params[:id]))
+  end
+
+  Endpoint.get('/chargeable_items')
+    .description("Get a list of Chargeable Items")
+    .params()
+    .permissions([])
+    .returns([200, "[(:chargeable_item)]"]) \
+  do
+    handle_unlimited_listing(ChargeableItem)
+  end
+end
---
+++ backend/controllers/chargeable_service.rb
@@ -0,0 +1,47 @@
+class ArchivesSpaceService < Sinatra::Base
+  Endpoint.post('/chargeable_services')
+    .description("Create a Chargeable Service")
+    .params(["chargeable_service", JSONModel(:chargeable_service), "The updated record", :body => true])
+    .permissions([])
+    .returns([200, :created]) \
+  do
+    handle_create(ChargeableService, params[:chargeable_service])
+  end
+
+  Endpoint.post('/chargeable_services/:id')
+    .description("Update a Chargeable Service")
+    .params(["id", :id],
+            ["chargeable_service", JSONModel(:chargeable_service), "The updated record", :body => true])
+    .permissions([])
+    .returns([200, :updated]) \
+  do
+    handle_update(ChargeableService, params[:id], params[:chargeable_service])
+  end
+
+  Endpoint.delete('/chargeable_services/:id')
+    .description("Delete a Chargeable Service")
+    .params(["id", :id])
+    .permissions([])
+    .returns([200, :deleted]) \
+  do
+    handle_delete(ChargeableService, params[:id])
+  end
+
+  Endpoint.get('/chargeable_services/:id')
+    .description("Get a Chargeable Service by ID")
+    .params(["id", :id])
+    .permissions([])
+    .returns([200, "(:chargeable_service)"]) \
+  do
+    json_response(ChargeableService.to_jsonmodel(params[:id]))
+  end
+
+  Endpoint.get('/chargeable_services')
+    .description("Get a list of Chargeable Services")
+    .params()
+    .permissions([])
+    .returns([200, "[(:chargeable_service)]"]) \
+  do
+    handle_unlimited_listing(ChargeableService)
+  end
+end
