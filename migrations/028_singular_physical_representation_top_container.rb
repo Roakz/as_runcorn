8391e593a6416445351eb18a5f2a999471d8f2bd
Mark Triggs <mark@dishevelled.net>
2019-06-06 15:44:52 +1000

    Link any existing physical representation to an arbitrary top container

--- migrations/028_singular_physical_representation_top_container.rb
+++ migrations/028_singular_physical_representation_top_container.rb
@@ -11,9 +11,9 @@ Sequel.migration do
     create_table(:representation_container_rlshp) do
       primary_key :id
 
-      Integer :physical_representation_id
-      Integer :top_container_id
-      Integer :aspace_relationship_position
+      Integer :physical_representation_id, :null => false
+      Integer :top_container_id, :null => false
+      Integer :aspace_relationship_position, :default => 0
 
       apply_mtime_columns(false)
     end
@@ -21,6 +21,27 @@ Sequel.migration do
     alter_table(:representation_container_rlshp) do
       add_foreign_key([:physical_representation_id], :physical_representation, :key => :id)
       add_foreign_key([:top_container_id], :top_container, :key => :id)
+    end
+
+    self.transaction do
+      # For want of a better idea, we'll link all existing representations to
+      # whatever top container comes to hand.
+      self[:repository].select(:id).each do |row|
+        repo_id = row[:id]
+
+        top_containers = self[:top_container].filter(:repo_id => repo_id).select(:id).map {|row| row[:id]}
+
+        unless top_containers.empty?
+          self[:physical_representation].filter(:repo_id => repo_id).select(:id).map {|row| row[:id]}.each do |phys_rep_id|
+            # whee!
+            self[:representation_container_rlshp].insert(:physical_representation_id => phys_rep_id,
+                                                         :top_container_id => top_containers.sample,
+                                                         :aspace_relationship_position => 0,
+                                                         :system_mtime => Time.now,
+                                                         :user_mtime => Time.now)
+          end
+        end
+      end
     end
   end
 
