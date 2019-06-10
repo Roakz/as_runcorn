980814b054277aa3c6f665a5133b6f148bdbf8ee
James Bullen <jambun@gmail.com>
2019-06-10 10:22:30 +1000

    Default top container current location to HOME

--- schemas/top_container_ext.rb
+++ schemas/top_container_ext.rb
@@ -1,4 +1,4 @@
 {
-  "current_location" => {"type" => "string", "dynamic_enum" => "runcorn_location", "ifmissing" => "error"},
+  "current_location" => {"type" => "string", "dynamic_enum" => "runcorn_location", "default" => "HOME"},
   "movements" => {"type" => "array", "items" => {"type" => "JSONModel(:movement) object"}},
 }
