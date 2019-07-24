baeb758feaab3daf174281e78fcb80342adfbdc7
Mark Triggs <mark@dishevelled.net>
2019-07-24 13:49:52 +1000

    Add the ability to add other record types to conservation requests

--- backend/model/conservation_request.rb
+++ backend/model/conservation_request.rb
@@ -5,11 +5,58 @@ class ConservationRequest < Sequel::Model(:conservation_request)
   set_model_scope :repository
 
   def add_representations(representation_model, *ids)
+    ids = ids.uniq
     backlink_col = :"#{representation_model.table_name}_id"
 
     DB.open do |db|
+      # Clear any existing entries so we don't end up with duplicates
+      db[:conservation_request_representations].filter(backlink_col => ids, :conservation_request_id => self.id).delete
       db[:conservation_request_representations].multi_insert(ids.map {|id| {backlink_col => id, :conservation_request_id => self.id}})
     end
+  end
+
+  def add_archival_objects(*archival_object_ids)
+    representation_ids = {
+      PhysicalRepresentation => [],
+      DigitalRepresentation => [],
+    }
+
+    DB.open do |db|
+      queue = archival_object_ids.clone
+
+      while !queue.empty?
+        # For this set of IDs, find any attached representations
+        representation_ids.keys.each do |representation_type|
+          representation_type.filter(:archival_object_id => queue).select(:id).each do |row|
+            representation_ids.fetch(representation_type) << row[:id]
+          end
+        end
+
+        # And continue the search with any children of our AO set
+        queue = db[:archival_object].filter(:parent_id => queue).select(:id).map {|row| row[:id]}
+      end
+    end
+
+    representation_ids.each do |representation_model, ids|
+      self.add_representations(representation_model, *ids)
+    end
+  end
+
+  def add_resources(*resource_ids)
+    top_level_ao_ids = ArchivalObject
+                         .filter(:parent_id => nil, :root_record_id => Resource.filter(:id => resource_ids).select(:id))
+                         .select(:id).map {|row| row[:id]}
+
+    self.add_archival_objects(*top_level_ao_ids)
+  end
+
+  def add_top_containers(*top_container_ids)
+    representation_ids = PhysicalRepresentation
+                           .find_relationship(:representation_container)
+                           .find_by_participant_ids(TopContainer, top_container_ids)
+                           .map {|relationship| relationship[:physical_representation_id]}
+
+    self.add_representations(PhysicalRepresentation, *representation_ids)
   end
 
 end
--- backend/spec/model_conservation_request_spec.rb
+++ backend/spec/model_conservation_request_spec.rb
@@ -2,33 +2,43 @@ require 'spec_helper'
 
 describe 'Runcorn Conservation Requests' do
 
+  let!(:conservation_request) { ConservationRequest.get_or_die(create(:json_conservation_request).id) }
+  let!(:top_container) { create(:json_top_container) }
+
+  let!(:series) { create(:json_resource) }
+
+  let!(:top_level_ao) {
+    create(:json_archival_object, :resource => {'ref' => series.uri})
+  }
+
+  let!(:ao_with_representations) {
+    create(:json_archival_object,
+           :resource => {'ref' => series.uri},
+           :parent => {'ref' => top_level_ao.uri},
+           :physical_representations => [
+             {
+               "title" => "bad song",
+               "description" => "Let us get physical!",
+               "current_location" => "N/A",
+               "normal_location" => "N/A",
+               "format" => "Drafting Cloth (Linen)",
+               "contained_within" => "OTH",
+               "container" => {"ref" => top_container.uri},
+             }
+           ],
+           :digital_representations => [
+             {
+               "title" => "bad song",
+               "description" => "Let us get digital!",
+               "file_type" => "JPEG",
+               "contained_within" => "Floppy Disk",
+               "normal_location" => "N/A",
+             }
+           ])
+  }
+
   it "lets you link representations to it directly" do
-    conservation_request = ConservationRequest.get_or_die(create(:json_conservation_request).id)
-
-    top_container = create(:json_top_container)
-
-    ao_obj = create(:json_archival_object,
-                    :physical_representations => [
-                      {
-                        "title" => "bad song",
-                        "description" => "Let us get physical!",
-                        "current_location" => "N/A",
-                        "normal_location" => "N/A",
-                        "format" => "Drafting Cloth (Linen)",
-                        "contained_within" => "OTH",
-                        "container" => {"ref" => top_container.uri},
-                      }
-                    ],
-                    :digital_representations => [
-                      {
-                        "title" => "bad song",
-                        "description" => "Let us get digital!",
-                        "file_type" => "JPEG",
-                        "contained_within" => "Floppy Disk",
-                        "normal_location" => "N/A",
-                      }
-                    ])
-    ao = ArchivalObject.to_jsonmodel(ao_obj.id)
+    ao = ArchivalObject.to_jsonmodel(ao_with_representations.id)
 
     physical_representation = ao.physical_representations.fetch(0)
     digital_representation = ao.digital_representations.fetch(0)
@@ -37,14 +47,44 @@ describe 'Runcorn Conservation Requests' do
     conservation_request.add_representations(DigitalRepresentation, JSONModel.parse_reference(digital_representation.fetch('uri')).fetch(:id))
 
     # And now I can see the conservation request on the representations in question
-    ao = ArchivalObject.to_jsonmodel(ao_obj.id)
-
-    # ao
-    require 'pp';$stderr.puts("\n*** DEBUG #{(Time.now.to_f * 1000).to_i} [model_conservation_request_spec.rb:43 e9daf]: " + {%Q^ao^ => ao}.pretty_inspect + "\n")
+    ao = ArchivalObject.to_jsonmodel(ao_with_representations.id)
 
     ao.physical_representations.dig(0, 'conservation_requests', 0, 'ref').should eq(conservation_request.uri)
     ao.digital_representations.dig(0, 'conservation_requests', 0, 'ref').should eq(conservation_request.uri)
+  end
 
+  it "accepts all representations under an AO" do
+    conservation_request.add_archival_objects(top_level_ao.id)
+
+    # Same deal as above: the representations have been linked to our
+    # conservation request.
+    ao = ArchivalObject.to_jsonmodel(ao_with_representations.id)
+
+    ao.physical_representations.dig(0, 'conservation_requests', 0, 'ref').should eq(conservation_request.uri)
+    ao.digital_representations.dig(0, 'conservation_requests', 0, 'ref').should eq(conservation_request.uri)
+  end
+
+  it "accepts all representations under a series" do
+    conservation_request.add_resources(series.id)
+
+    # Same deal as above: the representations have been linked to our
+    # conservation request.
+    ao = ArchivalObject.to_jsonmodel(ao_with_representations.id)
+
+    ao.physical_representations.dig(0, 'conservation_requests', 0, 'ref').should eq(conservation_request.uri)
+    ao.digital_representations.dig(0, 'conservation_requests', 0, 'ref').should eq(conservation_request.uri)
+  end
+
+  it "accepts all representations in a top container" do
+    conservation_request.add_top_containers(top_container.id)
+
+    ao = ArchivalObject.to_jsonmodel(ao_with_representations.id)
+
+    ao.physical_representations.dig(0, 'conservation_requests', 0, 'ref').should eq(conservation_request.uri)
+
+    # Digital representations aren't in top containers, so this one gets skipped
+    # over.
+    ao.digital_representations.dig(0, 'conservation_requests').should eq([])
   end
 
 end
