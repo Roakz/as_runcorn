92dd397e277ce8a918365f54481bbbf158cf2fed
James Bullen <jambun@gmail.com>
2019-08-03 12:06:55 +1000

    Bump lock_version on AOs when applying significance for history. Thanks PG

--- backend/model/mixins/significance.rb
+++ backend/model/mixins/significance.rb
@@ -22,14 +22,18 @@ module Significance
     PhysicalRepresentation.filter(:archival_object_id => self.id)
                           .filter(Sequel.~(:significance_id => sig_id))
                           .filter(:significance_is_sticky => 0)
-                          .update(:significance_id => sig_id, :system_mtime => Time.now)
+                          .update(:significance_id => sig_id,
+                                  :system_mtime => Time.now)
 
     changing_children = self.children.filter(Sequel.~(:significance_id => sig_id)).filter(:significance_is_sticky => 0)
 
     changing_ids = changing_children.select(:id).all.map{|r| r[:id]}
 
     unless changing_ids.empty?
-      changing_children.update(:significance_id => sig_id, :system_mtime => Time.now)
+      changing_children.update(:significance_id => sig_id,
+                               :lock_version => Sequel.expr(1) + :lock_version,
+                               :system_mtime => Time.now)
+
       self.class.filter(:id => changing_ids).each{|cc| cc.apply_significance!(sig_id)}
     end
   end
