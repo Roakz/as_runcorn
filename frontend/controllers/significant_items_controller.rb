ba5e407e61f6a7330da916bc38c682f55bfee11c
James Bullen <jambun@gmail.com>
2019-08-07 14:05:37 +1000

    Frontend support for showing significant items for a series

--- frontend/controllers/significant_items_controller.rb
+++ frontend/controllers/significant_items_controller.rb
@@ -3,7 +3,7 @@ class SignificantItemsController < ApplicationController
   set_access_control "view_repository" => [:index]
 
   def index
-    @significant_items = JSONModel::HTTP.get_json("/repositories/#{session[:repo_id]}/significant_items/all")
+    @significant_items = JSONModel::HTTP.get_json("/repositories/#{session[:repo_id]}/significant_items/all", 'series[]' => [params[:series_uri]])
   end
 
 end
