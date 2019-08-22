75e99c84391b3af2c51de5cc23dd6429698bb214
Payten Giles <payten.giles@gmail.com>
2019-08-22 10:55:31 +1000

    Fix typo in 055_rap_applied_performance.rb

--- migrations/055_rap_applied_performance.rb
+++ migrations/055_rap_applied_performance.rb
@@ -5,7 +5,7 @@ Sequel.migration do
   up do
     alter_table(:rap_applied) do
       add_column(:root_record_id, Integer, :null => true)
-      add_index([:root_record_id, :is_active], :unique => false, name => "rap_root_record_active_idx")
+      add_index([:root_record_id, :is_active], :unique => false, :name => "rap_root_record_active_idx")
     end
   end
 
