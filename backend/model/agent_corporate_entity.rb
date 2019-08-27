598c0d38b8f0892d67e3a44a2d815042f0f4fd32
Payten Giles <payten.giles@gmail.com>
2019-08-27 16:43:19 +1000

    Alias AgentCorporateEntity.validate so we don't get into trouble

--- backend/model/agent_corporate_entity.rb
+++ backend/model/agent_corporate_entity.rb
@@ -4,7 +4,11 @@ AgentCorporateEntity.include(AgencyDeletionRules)
 AgentCorporateEntity.include(ExternalReferences)
 
 class AgentCorporateEntity
+
+  alias_method :as_runcorn_validate_orig, :validate
   def validate
+    as_runcorn_validate_orig
+
     if self.publish == 1 && self.registration_state != 'approved'
       errors.add(:publish, "Unable to publish when in draft")
     end
