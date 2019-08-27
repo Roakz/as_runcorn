47fc3fed40452f8a038fbd11e066bd894082c9c0
Mark Triggs <mark@dishevelled.net>
2019-08-27 16:52:56 +1000

    Build deaccessioned map for AOs as well

--- backend/model/physical_representation.rb
+++ backend/model/physical_representation.rb
@@ -72,64 +72,6 @@ class PhysicalRepresentation < Sequel::Model(:physical_representation)
     super
   end
 
-  def self.build_deaccessioned_map(ao_ids)
-    deaccession_map = {}
-    parent_map = {}
-
-    ArchivalObject
-      .join(:resource, Sequel.qualify(:resource, :id) => Sequel.qualify(:archival_object, :root_record_id))
-      .left_join(Sequel.as(:deaccession, :deaccession_resource),
-                 Sequel.qualify(:deaccession_resource, :resource_id) => Sequel.qualify(:resource, :id))
-      .left_join(Sequel.as(:deaccession, :deaccession_ao),
-                 Sequel.qualify(:deaccession_ao, :archival_object_id) => Sequel.qualify(:archival_object, :id))
-      .filter(Sequel.qualify(:archival_object, :id) => ao_ids)
-      .select(Sequel.qualify(:archival_object, :id),
-              Sequel.qualify(:archival_object, :parent_id),
-              Sequel.as(Sequel.qualify(:deaccession_resource, :id), :resource_deaccession_id),
-              Sequel.as(Sequel.qualify(:deaccession_ao, :id), :ao_deaccession_id))
-      .each do |row|
-      deaccession_map[row[:id]] = !!(row[:resource_deaccession_id] || row[:ao_deaccession_id])
-      parent_map[row[:id]] = row[:parent_id]
-    end
-
-    if deaccession_map.values.any? {|val| !val}
-      # up the tree we go!
-      ids_to_process = parent_map.values.compact.uniq
-      while(!ids_to_process.empty?) do
-        next_ids_to_process = []
-        ArchivalObject
-          .left_join(Sequel.as(:deaccession, :deaccession_ao),
-                     Sequel.qualify(:deaccession_ao, :archival_object_id) => Sequel.qualify(:archival_object, :id))
-          .filter(Sequel.qualify(:archival_object, :id) => ids_to_process)
-          .select(Sequel.qualify(:archival_object, :id),
-                  Sequel.qualify(:archival_object, :parent_id),
-                  Sequel.as(Sequel.qualify(:deaccession_ao, :id), :ao_deaccession_id))
-          .each do |row|
-          parent_map[row[:id]] = row[:parent_id]
-
-          if row[:ao_deaccession_id]
-            deaccession_map[row[:id]] = true
-          else
-            next_ids_to_process << row[:parent_id]
-          end
-        end
-
-        ids_to_process = next_ids_to_process.compact.uniq
-      end
-    end
-
-    Hash[ao_ids.map {|ao_id|
-           current = ao_id
-           deaccessioned = false
-
-           while(current && !deaccessioned) do
-             deaccessioned = deaccession_map.fetch(current)
-             current = parent_map[current]
-           end
-
-           [ao_id, deaccessioned]
-         }]
-  end
 
   def self.sequel_to_jsonmodel(objs, opts = {})
     jsons = super
@@ -154,11 +96,12 @@ class PhysicalRepresentation < Sequel::Model(:physical_representation)
     assessments_map = build_assessments_map(objs)
 
     controlling_records_qsa_id_map = build_controlling_records_qsa_id_map(controlling_records_by_representation_id)
+
     controlling_records_dates_map = build_controlling_records_dates_map(controlling_records_by_representation_id)
 
     availability_options = get_sorted_availability_options
 
-    deaccessioned_map = build_deaccessioned_map(controlling_records_by_representation_id.values.map(&:id))
+    deaccessioned_map = Deaccessioned.build_deaccessioned_map(controlling_records_by_representation_id.values.map(&:id))
 
     objs.zip(jsons).each do |obj, json|
       json['existing_ref'] = obj.uri
