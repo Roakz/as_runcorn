d64d6ba8d17746c4ac223faa06207dc1410c336f
Sean Anderson <sean.anderson@gaiaresources.com.au>
2019-09-04 09:38:20 +0800

    * Resize title fields to fit a1 data

---
+++ migrations/064_resize_title_fields.rb
@@ -0,0 +1,17 @@
+require 'db/migrations/utils'
+
+Sequel.migration do
+
+  up do
+    alter_table(:physical_representation) do
+      set_column_type :title, :varchar, :size=>300, :null => true
+    end
+    alter_table(:digital_representation) do
+      set_column_type :title, :varchar, :size=>300, :null => true
+    end
+  end
+
+  down do
+  end
+
+end
