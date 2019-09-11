bd6ae30396d0176237e4364b5710c74ddd35559b
jambun <jambun@gmail.com>
2019-09-11 11:37:26 -0400

    Updated top_container bulk location update functions to support movements

--- backend/model/top_container.rb
+++ backend/model/top_container.rb
@@ -1,4 +1,65 @@
-TopContainer.prepend(RepresentationContainers)
-TopContainer.include(Movements)
-TopContainer.move_to_storage_permitted
-TopContainer.include(ContentsAwareness)
+class TopContainer
+  prepend RepresentationContainers
+  include Movements
+  move_to_storage_permitted
+  include ContentsAwareness
+
+  # replacements for:
+  #   self.bulk_update_location(ids, location_uri)
+  #   self.bulk_update_locations(location_data)
+  # movements require a user uri so adding it to the sigs
+  # the endpoints now call these versions
+
+  def self.bulk_update_location(ids, location_uri, user_uri)
+    out = {:records_updated => ids.length}
+
+    opts = {
+      :user => user_uri,
+      :location => location_uri
+    }
+
+    begin
+      ids.each do |id|
+        TopContainer[id].move(opts)
+      end
+    rescue
+      Log.exception($!)
+
+      out[:records_updated] = 0
+      out[:error] = $!
+    end
+
+    out
+  end
+
+
+  def self.bulk_update_locations(location_data, user_uri)
+    out = {
+      :records_ids_updated => []
+    }
+
+    opts = {
+      :user => user_uri
+    }
+
+    ids = location_data.map{|uri,_| my_jsonmodel.id_for(uri)}
+
+    location_data.each do |uri, location_uri|
+      id = my_jsonmodel.id_for(uri)
+      
+      begin
+        TopContainer[id].move(opts.merge(:location => location_uri))
+
+        out[:records_ids_updated] << id
+      rescue
+        Log.exception($!)
+
+        out[:error] = $!
+      end
+    end
+
+    out[:records_updated] = out[:records_ids_updated].length
+
+    out
+  end
+end
