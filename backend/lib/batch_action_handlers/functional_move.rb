1639697add375ea5e4f29465e783e7b84671e020
jambun <jambun@gmail.com>
2019-10-09 12:43:57 -0400

    Make functional move batch action databasey rather than jsony for performance etc

--- backend/lib/batch_action_handlers/functional_move.rb
+++ backend/lib/batch_action_handlers/functional_move.rb
@@ -25,28 +25,79 @@ class FunctionalMove < BatchActionHandler
   def self.perform_action(params, user, action_uri, uris)
     validate_params(params)
 
-    user_uri = JSONModel::JSONModel(:agent_person).uri_for(User.filter(:username => user).get(:agent_record_id))
+    username = RequestContext.get(:current_username)
+    user_agent_id = User[:username => username][:agent_record_id]
+    location_id = BackendEnumSource.id_for_value('runcorn_location', params['location'])
 
-    opts = {
-      :user => user_uri,
-      :location => params['location'],
-      :context => action_uri,
+    now = Time.now
+
+    movement_row = {
+      :top_container_id => 'SETME',
+      :physical_representation_id => 'SETME',
+      :storage_location_id => nil,
+      :functional_location_id => location_id,
+      :context_uri => action_uri,
+      :move_date => Date.today.strftime('%Y-%m-%d'),
+      :json_schema_version => 1,
+      :lock_version => 0,
+      :created_by => username,
+      :last_modified_by => username,
+      :create_time => now,
+      :system_mtime => now,
+      :user_mtime => now,
+    }
+
+    movement_user_rlshp_row = {
+      :movement_id => 'SETME',
+      :agent_person_id => user_agent_id,
+      :aspace_relationship_position => 0,
+      :created_by => username,
+      :last_modified_by => username,
+      :system_mtime => now,
+      :user_mtime => now,
     }
 
     models = ASModel.all_models.select {|model| model.included_modules.include?(Movements)}
 
     count = 0
 
-    begin
+    ids = {:top_container => [], :physical_representation => []}
+
+    DB.open do |db|
       uris.each do |uri|
         ref = JSONModel.parse_reference(uri)
-        model = models.select{|model| model.my_jsonmodel.record_type == ref[:type]}.first
-        model[ref[:id]].move(opts)
+
+        if ref[:type] == 'top_container'
+          ids[:top_container].push(ref[:id])
+          movement_row[:top_container_id] = ref[:id]
+          movement_row[:physical_representation_id] = nil
+        elsif ref[:type] == 'physical_representation'
+          ids[:physical_representation].push(ref[:id])
+          movement_row[:top_container_id] = nil
+          movement_row[:physical_representation_id] = ref[:id]
+        else
+          raise InvalidParams.new('Unknown uri type: ' + ref[:type])
+        end
+
+        movement_id = db[:movement].insert(movement_row)
+
+        movement_user_rlshp_row[:movement_id] = movement_id
+        db[:movement_user_rlshp].insert(movement_user_rlshp_row)
+
         count += 1
       end
-    rescue => e
-      # FIXME: think about exception handling
-      raise e
+
+      # update location of affected records and touch for indexing
+      ids.each do |model, id_list|
+        unless id_list.empty?
+          db[model].filter(:id => id_list)
+            .update(:current_location_id => location_id, :lock_version => Sequel.expr(1) + :lock_version, :system_mtime => now)
+        end
+      end
+
+      # bump lock_version for affected AOs for history
+      linked_ao_ids = db[:physical_representation].filter(:id => ids[:physical_representation]).select(:archival_object_id)
+      ArchivalObject.filter(:id => linked_ao_ids).update(:lock_version => Sequel.expr(1) + :lock_version, :system_mtime => now)
     end
 
     "#{count} object#{count == 1 ? '' : 's'} moved to #{I18n.t('enumerations.runcorn_location.' + params['location'])}."
