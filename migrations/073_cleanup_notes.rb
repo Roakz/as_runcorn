7ff147149a6100b946418abb2d9ed73b36fa4846
Payten Giles <payten.giles@gmail.com>
2019-10-25 14:31:36 +1100

    Fix indexer failing with validation errors after note schema changes.
    QSA has a customisation that only note_singlepart is supported for Agents
    and the type enumeration has been customised also
    
    Drop all agent or singlepart notes so we can create only the pure notes
    we desire

---
+++ migrations/073_cleanup_notes.rb
@@ -0,0 +1,35 @@
+require 'db/migrations/utils'
+
+Sequel.migration do
+
+  up do
+    # QSA has a customisation that only note_singlepart is supported for Agents
+    # and the type enumeration has been customised also
+    #
+    # Drop all agent or singlepart notes so we can create only the pure notes
+    # we desire
+    note_ids =  self[:note]
+                  .filter(Sequel.|(Sequel.~(:agent_person_id => nil),
+                                   Sequel.~(:agent_corporate_entity_id => nil),
+                                   Sequel.~(:agent_family_id => nil),
+                                   Sequel.~(:agent_software_id => nil),
+                                   Sequel.like(:notes, '{"jsonmodel_type":"note_singlepart"%')))
+                  .select(:id)
+
+    self[:note_persistent_id]
+      .filter(:note_id => note_ids)
+      .delete
+
+    self[:subnote_metadata]
+      .filter(:note_id => note_ids)
+      .delete
+
+    self[:note]
+      .filter(:id => note_ids)
+      .delete
+  end
+
+  down do
+  end
+
+end
