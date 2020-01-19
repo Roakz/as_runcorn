75a7cfe508fe34173efdaecd39f319ec4d1f92fa
Payten Giles <payten.giles@gmail.com>
2020-01-20 09:24:01 +1100

    Add missing backend endpoint for AgencyTransferProposalsReport

--- backend/controllers/runcorn_reports.rb
+++ backend/controllers/runcorn_reports.rb
@@ -34,6 +34,23 @@ class ArchivesSpaceService < Sinatra::Base
     ]
   end
 
+  Endpoint.get_or_post('/runcorn_reports/transfer_proposals')
+    .description("Report on Agency Transfer Proposals")
+    .params(["from_date", String, "From date in range", :optional => true],
+            ["to_date", String, "To date in range", :optional => true])
+    .permissions([]) # FIXME
+    .returns([200, "(:csv)"]) \
+  do
+    [
+      200,
+      {
+        "Content-Type" => "text/csv",
+        "Content-Disposition" => "attachment; filename=\"transfer_proposals.#{Date.today.iso8601}.csv\""
+      },
+      AgencyTransferProposalsReport.new(DateParse.date_parse_down(params[:from_date]), DateParse.date_parse_up(params[:to_date])).to_stream
+    ]
+  end
+
   Endpoint.get_or_post('/runcorn_reports/file_issue_invoices')
     .description("Produce invoice for file issues")
     .params(["from_date", String, "From date in range", :optional => true],
