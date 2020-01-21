6b2e517c065cccf49ea661561f2e266f64e3d4b3
Mark Triggs <mark@dishevelled.net>
2020-01-22 10:21:25 +1100

    No need to mess with the administrators group if it's not defined yet

--- migrations/092_refiddle_permissions.rb
+++ migrations/092_refiddle_permissions.rb
@@ -243,26 +243,28 @@ Sequel.migration do
 
     admin_group_id = self[:group].filter(:group_code => 'administrators').get(:id)
 
-    new_perms.each do |perm|
-      perm_id = self[:permission].insert(
-                                         :permission_code => perm[:code],
-                                         :description => perm[:description],
-                                         :level => 'repository',
-                                         :system => 0,
-                                         :create_time => now,
-                                         :system_mtime => now,
-                                         :user_mtime => now
-                                         )
+    unless admin_group_id.nil?
+      new_perms.each do |perm|
+        perm_id = self[:permission].insert(
+          :permission_code => perm[:code],
+          :description => perm[:description],
+          :level => 'repository',
+          :system => 0,
+          :create_time => now,
+          :system_mtime => now,
+          :user_mtime => now
+        )
 
-      self[:group_permission].insert(:permission_id => perm_id, :group_id => admin_group_id)
+        self[:group_permission].insert(:permission_id => perm_id, :group_id => admin_group_id)
 
-      self[:repository].where{id > 1}.each do |repo|
-        repo_id = repo[:id]
+        self[:repository].where{id > 1}.each do |repo|
+          repo_id = repo[:id]
 
-        perm[:groups].each do |group_code|
-          group_id = self[:group].filter(:repo_id => repo_id, :group_code => group_code).get(:id)
+          perm[:groups].each do |group_code|
+            group_id = self[:group].filter(:repo_id => repo_id, :group_code => group_code).get(:id)
 
-          self[:group_permission].insert(:permission_id => perm_id, :group_id => group_id)
+            self[:group_permission].insert(:permission_id => perm_id, :group_id => group_id)
+          end
         end
       end
     end
--- migrations/095_another_permission_fiddle.rb
+++ migrations/095_another_permission_fiddle.rb
@@ -11,39 +11,41 @@ Sequel.migration do
 
     # create manage_closed_record_approval with groups
     new_perms = [
-                 {
-                   :code => 'manage_closed_record_approval',
-                   :description => 'The ability to manage agency approval of closed records',
-                   :groups => ['repository-senior-archivists', 'repository-collection-managers',
-                               'repository-collection-archivists', 'repository-archivists-other',
-                               'repository-reading-room-staff']
-                 },
-                ]
+      {
+        :code => 'manage_closed_record_approval',
+        :description => 'The ability to manage agency approval of closed records',
+        :groups => ['repository-senior-archivists', 'repository-collection-managers',
+                    'repository-collection-archivists', 'repository-archivists-other',
+                    'repository-reading-room-staff']
+      },
+    ]
 
     now = Time.now
 
     admin_group_id = self[:group].filter(:group_code => 'administrators').get(:id)
 
-    new_perms.each do |perm|
-      perm_id = self[:permission].insert(
-                                         :permission_code => perm[:code],
-                                         :description => perm[:description],
-                                         :level => 'repository',
-                                         :system => 0,
-                                         :create_time => now,
-                                         :system_mtime => now,
-                                         :user_mtime => now
-                                         )
+    unless admin_group_id.nil?
+      new_perms.each do |perm|
+        perm_id = self[:permission].insert(
+          :permission_code => perm[:code],
+          :description => perm[:description],
+          :level => 'repository',
+          :system => 0,
+          :create_time => now,
+          :system_mtime => now,
+          :user_mtime => now
+        )
 
-      self[:group_permission].insert(:permission_id => perm_id, :group_id => admin_group_id)
+        self[:group_permission].insert(:permission_id => perm_id, :group_id => admin_group_id)
 
-      self[:repository].where{id > 1}.each do |repo|
-        repo_id = repo[:id]
+        self[:repository].where{id > 1}.each do |repo|
+          repo_id = repo[:id]
 
-        perm[:groups].each do |group_code|
-          group_id = self[:group].filter(:repo_id => repo_id, :group_code => group_code).get(:id)
+          perm[:groups].each do |group_code|
+            group_id = self[:group].filter(:repo_id => repo_id, :group_code => group_code).get(:id)
 
-          self[:group_permission].insert(:permission_id => perm_id, :group_id => group_id)
+            self[:group_permission].insert(:permission_id => perm_id, :group_id => group_id)
+          end
         end
       end
     end
