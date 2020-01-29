da72ee7f9d24790906cef715bdb453de9aea577c
James Bullen <jambun@gmail.com>
2020-01-29 17:22:48 +1100

    Show significant item counts in frontend

--- frontend/controllers/significant_items_controller.rb
+++ frontend/controllers/significant_items_controller.rb
@@ -5,6 +5,7 @@ class SignificantItemsController < ApplicationController
   def index
     @significant_items = JSONModel::HTTP.get_json("/repositories/#{session[:repo_id]}/significant_items/all",
                                                   'series[]' => [params[:series_uri]], 'location' => params[:location])
+    @counts = JSONModel::HTTP.get_json("/repositories/#{session[:repo_id]}/significant_item_counts")
   end
 
 end
--- frontend/helpers/significance_helper.rb
+++ frontend/helpers/significance_helper.rb
@@ -6,11 +6,12 @@ module SignificanceHelper
     'memory_of_the_world' => 'danger',
   }
 
-  def self.display(sig, count = false)
+  def self.display(sig, count = false, total = false)
     return unless sig
     out = '<span class="label label-' + SignificanceHelper::SIG_LABEL_MAP[sig] + '" style="font-size:small;margin-right:10px;">'
     out += I18n.t("enumerations.runcorn_significance.#{sig}")
     out += ': ' + count.to_s if count
+    out += ' of ' + total.to_s if count && total
     out << '</span>'
     out.html_safe
   end
