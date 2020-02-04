04782bb25062ae97a2628f198b1bbb233dede8eb
Payten Giles <payten@Payten.lan>
2020-02-04 15:14:32 +1100

    Piv#167638624 incomplete Assessments report

--- backend/model/reports/assessments_report.rb
+++ backend/model/reports/assessments_report.rb
@@ -18,7 +18,7 @@ class AssessmentsReport < RuncornReport
 
     CSV.open(tempfile, 'w') do |csv|
       csv << ['Assessement ID (AS)',
-              'Series ID (S)/ Title',
+              'Series ID (S)/Title',
               'Status',
               'Survey Start',
               'Survey End',
@@ -29,7 +29,7 @@ class AssessmentsReport < RuncornReport
               'Number of items']
 
       DB.open do |aspacedb|
-        base_ds = aspacedb[:conservation_request]
+        base_ds = aspacedb[:assessment]
 
         if @from_date
           from_time = @from_date.to_time.to_i * 1000
@@ -41,10 +41,51 @@ class AssessmentsReport < RuncornReport
           base_ds = base_ds.where { Sequel.qualify(:conservation_request, :create_time) <= to_time }
         end
 
+        resource_map = build_assessment_to_resources_map(base_ds)
+        conservation_issues_map = build_conservation_issues_map(base_ds)
+
         base_ds
+          .left_join(:conservation_request_assessment_rlshp, Sequel.qualify(:conservation_request_assessment_rlshp, :assessment_id) => Sequel.qualify(:assessment, :id))
+          .left_join(:conservation_request, Sequel.qualify(:conservation_request, :id) => Sequel.qualify(:conservation_request_assessment_rlshp, :conservation_request_id))
+          .select(Sequel.qualify(:assessment, :id),
+                  Sequel.qualify(:conservation_request, :status_id),
+                  Sequel.qualify(:assessment, :survey_begin),
+                  Sequel.qualify(:assessment, :survey_end),
+                  Sequel.qualify(:assessment, :treatment_priority_id),
+                  Sequel.qualify(:assessment, :surveyed_duration),
+                  )
           .each do |row|
-          csv << [
-          ]
+
+          if resource_map.include?(row[:id])
+            resource_map.fetch(row[:id]).each do |resource_data|
+              csv << [
+                QSAId.prefixed_id_for(Assessment, row[:id]),
+                '%s %s' % [resource_data.fetch(:qsa_id), resource_data.fetch(:title)],
+                BackendEnumSource.value_for_id('conservation_request_status', row[:status_id]),
+                row[:survey_begin],
+                row[:survey_end],
+                BackendEnumSource.value_for_id('runcorn_treatment_priority', row[:treatment_priority_id]),
+                'FIXME',
+                conservation_issues_map.fetch(row[:id], false) ? conservation_issues_map.fetch(row[:id]).join('; ') : nil,
+                row[:surveyed_duration],
+                resource_data.fetch(:count)
+              ]
+            end
+          else
+            # no items linked to assessment
+            csv << [
+              QSAId.prefixed_id_for(Assessment, row[:id]),
+              nil,
+              BackendEnumSource.value_for_id('conservation_request_status', row[:status_id]),
+              row[:survey_begin],
+              row[:survey_end],
+              BackendEnumSource.value_for_id('runcorn_treatment_priority', row[:treatment_priority_id]),
+              'FIXME',
+              conservation_issues_map.fetch(row[:id], false) ? conservation_issues_map.fetch(row[:id]).join('; ') : nil,
+              row[:surveyed_duration],
+              nil
+            ]
+          end
         end
       end
     end
@@ -53,4 +94,45 @@ class AssessmentsReport < RuncornReport
     tempfile
   end
 
+  def build_assessment_to_resources_map(base_ds)
+    result = {}
+
+    base_ds
+        .join(:assessment_rlshp, Sequel.qualify(:assessment_rlshp, :assessment_id) => Sequel.qualify(:assessment, :id))
+        .join(:physical_representation, Sequel.qualify(:physical_representation, :id) => Sequel.qualify(:assessment_rlshp, :physical_representation_id))
+        .join(:resource, Sequel.qualify(:resource, :id) => Sequel.qualify(:physical_representation, :resource_id))
+        .group_and_count(Sequel.qualify(:assessment, :id),
+                         Sequel.qualify(:physical_representation, :resource_id),
+                         Sequel.qualify(:resource, :qsa_id),
+                         Sequel.qualify(:resource, :title))
+        .each do |row|
+      result[row[:id]] ||= []
+      result[row[:id]] << {
+          :id => row[:resource_id],
+          :count => row[:count],
+          :qsa_id => QSAId.prefixed_id_for(Resource, row[:qsa_id]),
+          :title => row[:title],
+      }
+    end
+
+    result
+  end
+
+  def build_conservation_issues_map(base_ds)
+    result = {}
+
+    base_ds
+      .join(:assessment_attribute, Sequel.qualify(:assessment_attribute, :assessment_id) => Sequel.qualify(:assessment, :id))
+      .join(:assessment_attribute_definition, Sequel.qualify(:assessment_attribute_definition, :id) => Sequel.qualify(:assessment_attribute, :assessment_attribute_definition_id))
+      .filter(Sequel.qualify(:assessment_attribute, :value) => 'true')
+      .filter(Sequel.qualify(:assessment_attribute_definition, :type) => 'conservation_issue')
+      .select(Sequel.qualify(:assessment, :id),
+              Sequel.qualify(:assessment_attribute_definition, :label))
+      .each do |row|
+      result[row[:id]] ||= []
+      result[row[:id]] << row[:label]
+    end
+
+    result
+  end
 end
