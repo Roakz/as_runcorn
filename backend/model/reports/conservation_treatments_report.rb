af262c8eb6e52e637ef064995d619b4196e1b9d1
Payten Giles <payten.giles@gmail.com>
2020-02-06 09:03:38 +1100

    Piv#167638624 some more treatment report things

--- backend/model/reports/conservation_treatments_report.rb
+++ backend/model/reports/conservation_treatments_report.rb
@@ -49,6 +49,9 @@ class ConservationTreatmentsReport < RuncornReport
           base_ds = base_ds.where { Sequel.qualify(:conservation_treatment, :create_time) <= to_time }
         end
 
+        responsible_agency_map = build_responsible_agency(base_ds)
+        agency_qsa_ids = build_agency_qsa_id_map(aspacedb, responsible_agency_map)
+
         base_ds
           .left_join(:conservation_treatment_assessment_rlshp, Sequel.qualify(:conservation_treatment_assessment_rlshp, :conservation_treatment_id) => Sequel.qualify(:conservation_treatment, :id))
           .left_join(:assessment, Sequel.qualify(:assessment, :id) => Sequel.qualify(:conservation_treatment_assessment_rlshp, :assessment_id))
@@ -67,6 +70,7 @@ class ConservationTreatmentsReport < RuncornReport
               Sequel.as(Sequel.qualify(:resource, :title), :series_title),
               Sequel.as(Sequel.qualify(:physical_representation, :qsa_id), :pr_qsa_id),
               Sequel.as(Sequel.qualify(:physical_representation, :format_id), :pr_format_id),
+              Sequel.as(Sequel.qualify(:archival_object, :id), :archival_object_id),
               Sequel.as(Sequel.qualify(:archival_object, :qsa_id), :controlling_record_qsa_id),
               Sequel.qualify(:conservation_treatment, :status),
               Sequel.qualify(:conservation_treatment, :treatment_process),
@@ -86,7 +90,7 @@ class ConservationTreatmentsReport < RuncornReport
           csv << [
             row[:conservation_request_id] ? QSAId.prefixed_id_for(ConservationRequest, row[:conservation_request_id]) : nil,
             row[:assessment_id] ? QSAId.prefixed_id_for(Assessment, row[:assessment_id]) : nil,
-            'FIXME???',
+            agency_qsa_ids.fetch(responsible_agency_map.fetch(row[:archival_object_id]), nil),
             QSAId.prefixed_id_for(Resource, row[:series_qsa_id]),
             row[:series_title],
             QSAId.prefixed_id_for(ArchivalObject, row[:controlling_record_qsa_id]),
@@ -95,7 +99,7 @@ class ConservationTreatmentsReport < RuncornReport
             row[:status].gsub('_', ' '),
             row[:treatment_process],
             row[:treatments_applied],
-            'FIXME???',
+            row[:treatments_applied] ? row[:treatments_applied].split(',').reject{|s| s.strip.empty?}.length : 0,
             row[:materials_used_consumables],
             row[:materials_used_staff_time],
             row[:date_required_by],
@@ -111,4 +115,38 @@ class ConservationTreatmentsReport < RuncornReport
     tempfile
   end
 
+  def build_responsible_agency(base_ds)
+    result = {}
+
+    repo_id = base_ds
+                .join(:physical_representation, Sequel.qualify(:physical_representation, :id) => Sequel.qualify(:conservation_treatment, :physical_representation_id))
+                .select(:repo_id).first[:repo_id]
+
+    controlling_record_ids = base_ds
+                              .join(:physical_representation, Sequel.qualify(:physical_representation, :id) => Sequel.qualify(:conservation_treatment, :physical_representation_id))
+                              .select(Sequel.qualify(:physical_representation, :archival_object_id))
+                              .map{|row| row[:archival_object_id]}.uniq
+
+    RequestContext.open(:repo_id => repo_id) do
+      ArchivalObject
+        .filter(:id => base_ds
+                         .join(:physical_representation, Sequel.qualify(:physical_representation, :id) => Sequel.qualify(:conservation_treatment, :physical_representation_id))
+                         .select(Sequel.qualify(:physical_representation, :archival_object_id)))
+        .each do |ao|
+        result[ao.id] = JSONModel::JSONModel(:agent_corporate_entity).id_for(ao.responsible_agency.fetch(:uri))
+      end
+    end
+
+    result
+  end
+
+  def build_agency_qsa_id_map(aspacedb, responsible_agency_map)
+    agency_ids = responsible_agency_map.values.uniq
+    aspacedb[:agent_corporate_entity]
+      .filter(:id => agency_ids)
+      .select(:id, :qsa_id)
+      .map do |row|
+      [row[:id], QSAId.prefixed_id_for(AgentCorporateEntity, row[:qsa_id])]
+    end.to_h
+  end
 end
