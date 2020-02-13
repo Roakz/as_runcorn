29c3a49c5e4714b74f49d9a853f7f23e21a8ef7a
Payten Giles <payten.giles@gmail.com>
2020-02-13 14:58:53 +1100

    Piv#170671099 refactor search export override to allow for custom exports by type

---
+++ backend/model/csv_exports/base_csv_export.rb
@@ -0,0 +1,101 @@
+require_relative 'csv_export_record'
+
+class BaseCSVExport
+  attr_accessor :criteria, :repo_id
+
+  def initialize(criteria, repo_id)
+    @criteria = criteria
+    @repo_id = repo_id
+  end
+
+  Column = Struct.new(:heading, :value)
+
+  def columns
+    [
+      Column.new('Record Type', proc{|record| record.type}),
+      Column.new('ID', proc{|record| record.id}),
+      Column.new('Title', proc{|record| record.title}),
+      Column.new('Start Date', proc{|record| record.start_date}),
+      Column.new('Certainty', proc{|record| record.start_date_certainty}),
+      Column.new('End Date', proc{|record| record.end_date}),
+      Column.new('Certainty', proc{|record| record.end_date_certainty}),
+      Column.new('Found In', proc{|record| record.found_in}),
+      Column.new('Published?', proc{|record| record.published}),
+      Column.new('Archivist Approved?', proc{|record| record.archivist_approved}),
+      Column.new('Accessioned/ Retention Status', proc{|record| record.accessioned_retention_status}),
+      Column.new('Category/ Format', proc{|record| record.category_format}),
+      Column.new('Associated Record ID', proc{|record| record.associated_record_id}),
+      Column.new('Contained Within', proc{|record| record.contained_within}),
+      Column.new('Top Container', proc{|record| record.top_container}),
+      Column.new('RAP Status', proc{|record| record.rap_status}),
+      Column.new('RAP Expiry Date', proc{|record| record.rap_expiry_date}),
+      Column.new('Access Category', proc{|record| record.access_category}),
+      Column.new('RAP Publish Details?', proc{|record| record.rap_publish_details}),
+      Column.new('RAP is inherited?', proc{|record| record.rap_is_inherited}),
+      Column.new('Significance', proc{|record| record.significance}),
+      Column.new('Inherit Significance?', proc{|record| record.inherit_significance}),
+      Column.new('Sensitivity Label', proc{|record| record.sensitivity_label}),
+      Column.new('Agency Control No.', proc{|record| record.agency_control_number}),
+      Column.new('Previous System Location', proc{|record| record.previous_system_identifier}),
+      Column.new('Disposal Class', proc{|record| record.disposal_class}),
+      Column.new('Home Location', proc{|record| record.home_location}),
+      Column.new('Current Location', proc{|record| record.current_location}),
+      Column.new('Availability', proc{|record| record.availability}),
+      Column.new('Status', proc{|record| record.status}),
+      Column.new('Colour', proc{|record| record.colour}),
+      Column.new('File Size', proc{|record| record.file_size}),
+      Column.new('File Issue Allowed?', proc{|record| record.file_issue_allowed}),
+      Column.new('Exhibition Quality?', proc{|record| record.exhibition_quality}),
+      Column.new('Intended Use', proc{|record| record.intended_use}),
+      Column.new('Original Registration Date', proc{|record| record.original_registration_date}),
+      Column.new('Serialised?', proc{|record| record.serialised}),
+      Column.new('Accrual?', proc{|record| record.accrual}),
+      Column.new('Reason Requested', proc{|record| record.reason_requested}),
+      Column.new('Source', proc{|record| record.source}),
+      Column.new('Responsible Agency ID', proc{|record| record.responsible_agency_id}),
+      Column.new('Responsible Agency Name', proc{|record| record.responsible_agency_name}),
+      Column.new('Responsible Agency Inherited?', proc{|record| record.responsible_agency_inherited}),
+      Column.new('Repository', proc{|record| record.repository}),
+      Column.new('Floor', proc{|record| record.floor}),
+      Column.new('Room', proc{|record| record.room}),
+      Column.new('Area', proc{|record| record.area}),
+      Column.new('Location Profile', proc{|record| record.location_profile}),
+      Column.new('Treatment Status', proc{|record| record.treatment_status}),
+      Column.new('Treatments Applied', proc{|record| record.treatments_applied}),
+      Column.new('Date Commenced ', proc{|record| record.date_commenced}),
+      Column.new('Date Completed', proc{|record| record.date_completed}),
+      Column.new('Assessment ID', proc{|record| record.assessment_id}),
+      Column.new('Created Date', proc{|record| record.created_date}),
+      Column.new('Created By', proc{|record| record.created_by}),
+      Column.new('Last Modified Date', proc{|record| record.last_modified_date}),
+      Column.new('Last Modified By', proc{|record| record.last_modified_by}),
+    ]
+  end
+
+  def to_csv
+    tempfile = Tempfile.new('SearchExport')
+
+    CSV.open(tempfile, 'w') do |csv|
+      csv << columns.map(&:heading)
+
+      while(true) do
+        result = Search.search(criteria, repo_id)
+
+        break if Array(result['results']).empty?
+
+        result['results'].each do |doc|
+          json = ASUtils.json_parse(doc['json'])
+          record = CSVExportRecord.new(doc, json)
+          csv << columns.map {|col| col.value.call(record)}
+        end
+
+        break if result['last_page'] <= result['this_page']
+
+        criteria[:page] = criteria[:page] + 1
+      end
+    end
+
+    tempfile.rewind
+    tempfile
+  end
+end
---
+++ backend/model/csv_exports/csv_export_record.rb
@@ -0,0 +1,405 @@
+class CSVExportRecord
+  attr_accessor :doc, :json
+
+  def initialize(doc, json)
+    @doc = doc
+    @json = json
+  end
+
+  def type
+    doc['primary_type'].split('_').collect(&:capitalize).join(' ')
+  end
+
+  def id
+    if doc['primary_type'] == 'top_container' && json['identifier']
+      json['identifier']
+    elsif json['qsa_id_prefixed']
+      json['qsa_id_prefixed']
+    else
+      JSONModel::JSONModel(doc['primary_type'].intern).id_for(doc['id'])
+    end
+  end
+
+  def title
+    doc['title'] || doc['display_string']
+  end
+
+  def start_date
+    Array(doc['date_start_u_sstr']).first
+  end
+
+  def start_date_certainty
+    Array(doc['date_start_certainty_u_sstr']).first
+  end
+
+  def end_date
+    Array(doc['date_end_u_sstr']).first
+  end
+
+  def end_date_certainty
+    Array(doc['date_end_certainty_u_sstr']).first
+  end
+
+  def found_in
+    if ['physical_representation', 'digital_representation'].include?(doc['primary_type'])
+      json.dig('controlling_record_series', 'qsa_id_prefixed')
+    elsif doc['primary_type'] == 'archival_object'
+      Array(json['within']).find{|qsa_id| QSAId.parse_prefixed_id(qsa_id)[:model] == Resource}
+    end
+  end
+
+  def published
+    if json.has_key?('publish')
+      json['publish'] ? 'Y' : 'N'
+    else
+      nil
+    end
+  end
+
+  def archivist_approved
+    if json.has_key?('archivist_approved')
+      json['archivist_approved'] ? 'Y' : 'N'
+    else
+      nil
+    end
+  end
+
+  def accessioned_retention_status
+    if json['retention_status']
+      json['retention_status']
+    elsif json['accessioned_status']
+      json['accessioned_status']
+    end
+  end
+
+  def category_format
+    if json['agency_category']
+      json['agency_category']
+    elsif json['mandate_type']
+      json['mandate_type']
+    elsif json['format']
+      json['format']
+    elsif json['file_type']
+      json['file_type']
+    elsif doc['primary_type'] == 'top_container'
+      if json['container_profile']
+        json.dig('container_profile', '_resolved', 'name')
+      else
+        json['type']
+      end
+    elsif doc['primary_type'] == 'file_issue_request'
+      types = []
+      types << 'Physical' if json['physical_request_status'] != 'NONE_REQUESTED'
+      types << 'Digital' if json['digital_request_status'] != 'NONE_REQUESTED'
+
+      if type.length > 0
+        '%s %s' % [types.join('/'), 'File Issue Request']
+      end
+    elsif doc['primary_type'] == 'file_issue'
+      if json['issue_type'] == 'PHYSICAL'
+        'Physical File Issue'
+      else
+        'Digital File Issue'
+      end
+    elsif doc['primary_type'] == 'transfer_proposal'
+      if Array(json['series']).length > 0
+        Array(json['series']).map{|series| series['composition']}.flatten.uniq.sort.join('/')
+      end
+    elsif doc['primary_type'] == 'transfer'
+      if Array(json.dig('transfer_proposal', '_resolved', 'series')).length > 0
+        Array(json.dig('transfer_proposal', '_resolved', 'series')).map{|series| series['composition']}.flatten.uniq.sort.join('/')
+      end
+    elsif json['treatment_priority']
+      json['treatment_priority']
+    elsif doc['primary_type'] == 'reading_room_request'
+      json.dig('requested_item', '_resolved', 'rap_access_status')
+    elsif doc['primary_type'] == 'agency_reading_room_request'
+      json.dig('requested_item', '_resolved', 'rap_access_status')
+    elsif doc['primary_type'] == 'item_use'
+      json['item_use_type']
+    elsif doc['primary_type'] == 'subject'
+      json['terms'].map{|term| term['term_type']}.uniq.join('; ')
+    end
+  end
+
+  def associated_record_id
+    if ['physical_representation', 'digital_representation'].include?(doc['primary_type'])
+      json.dig('controlling_record', 'qsa_id_prefixed')
+    elsif doc['primary_type'] == 'archival_object'
+      Array(json['within']).find{|qsa_id| QSAId.parse_prefixed_id(qsa_id)[:model] == ArchivalObject} ||
+        Array(json['within']).find{|qsa_id| QSAId.parse_prefixed_id(qsa_id)[:model] == Resource}
+    elsif doc['primary_type'] == 'file_issue'
+      if json.dig('file_issue_request', 'ref')
+        QSAId.prefixed_id_for(FileIssueRequest, JSONModel::JSONModel(:file_issue_request).id_for(json.dig('file_issue_request', 'ref')))
+      end
+    elsif doc['primary_type'] == 'transfer'
+      json.dig('transfer_proposal', '_resolved', 'qsa_id_prefixed')
+    elsif doc['primary_type'] == 'assessment'
+      if json['conservation_request_id']
+        QSAId.prefixed_id_for(ConservationRequest, json['conservation_request_id'])
+      end
+    elsif doc['primary_type'] == 'reading_room_request'
+      json.dig('requested_item', '_resolved', 'qsa_id_prefixed')
+    elsif doc['primary_type'] == 'agency_reading_room_request'
+      json.dig('requested_item', '_resolved', 'qsa_id_prefixed')
+    end
+  end
+
+  def contained_within
+    json['contained_within']
+  end
+
+  def top_container
+    if doc['primary_type'] == 'physical_representation'
+      json.dig('container', '_resolved', 'indicator')
+    end
+  end
+
+  def rap_status
+    json['rap_access_status']
+  end
+
+  def rap_expiry_date
+    if json['rap_expiration']
+      if json['expires']
+        json.dig('rap_expiration', 'expiry_date')
+      else
+        'No expiry'
+      end
+    end
+  end
+
+  def access_category
+    if json['rap_applied']
+      json.dig('rap_applied', 'access_category')
+    elsif json['rap_attached']
+      json.dig('rap_attached', 'access_category')
+    end
+  end
+
+  def rap_publish_details
+    if json['rap_applied']
+      json.dig('rap_applied', 'open_access_metadata') ? 'Y' : 'N'
+    elsif json['rap_attached']
+      json.dig('rap_attached', 'open_access_metadata') ? 'Y' : 'N'
+    end
+  end
+
+  def rap_is_inherited
+    if json['rap_applied'] && json['rap_attached']
+      json.dig('rap_applied', 'uri') == json.dig('rap_attached', 'uri') ? 'N' : 'Y'
+    elsif json['rap_applied']
+      'N'
+    end
+  end
+
+  def significance
+    json['significance']
+  end
+
+  def inherit_significance
+    if json.has_key?('significance_is_sticky')
+      if json['significance_is_sticky']
+        'Y'
+      else
+        'N'
+      end
+    end
+  end
+
+  def sensitivity_label
+    json['sensitivity_label']
+  end
+
+  def agency_control_number
+    json['agency_assigned_id']
+  end
+
+  def previous_system_identifier
+    json['previous_system_identifiers']
+  end
+
+  def disposal_class
+    json['disposal_class']
+  end
+
+  def home_location
+    Array(doc['top_container_home_location_u_sstr']).first
+  end
+
+  def current_location
+    Array(doc['current_location_u_sstr']).first
+  end
+
+  def availability
+    json['calculated_availability']
+  end
+
+  def status
+    if json['status']
+      json['status']
+    elsif doc['primary_type'] == 'file_issue_request'
+      '%s: %s; %s: %s' % ['Physical', json['physical_request_status'], 'Digital', json['digital_request_status']]
+    end
+  end
+
+  def colour
+    json['colour']
+  end
+
+  def file_size
+    json['file_size']
+  end
+
+  def file_issue_allowed
+    if json.has_key?('file_issue_allowed')
+      return 'Y' if json['file_issue_allowed'] == 'true'
+      return 'N' if json['file_issue_allowed'] == 'false'
+      json['file_issue_allowed']
+    end
+  end
+
+  def exhibition_quality
+    if json.has_key?('exhibition_quality')
+      json['exhibition_quality'] ? 'Y' : 'N'
+    end
+  end
+
+  def intended_use
+    json['intended_use']
+  end
+
+  def original_registration_date
+    json['original_registration_date']
+  end
+
+  def serialised
+    if json['serialised']
+      json['serialised'] ? 'Y' : 'N'
+    end
+  end
+
+  def accrual
+    if json['accrual']
+      json['accrual'] ? 'Y' : 'N'
+    end
+  end
+
+  def reason_requested
+    if doc['primary_type'] == 'file_issue_request'
+      json['request_type']
+    elsif doc['primary_type'] == 'file_issue'
+      json['request_type']
+    elsif doc['primary_type'] == 'search_request'
+      json['purpose']
+    elsif doc['primary_type'] == 'conservation_request'
+      json['reason_requested']
+    end
+  end
+
+  def source
+    if doc['primary_type'] == 'function'
+      json['source']
+    elsif doc['primary_type'] == 'subject'
+      json['source']
+    end
+  end
+
+  def responsible_agency_id
+    json.dig('responsible_agency', '_resolved', 'qsa_id_prefixed')
+  end
+
+  def responsible_agency_name
+    json.dig('responsible_agency', '_resolved', 'title')
+  end
+
+  def responsible_agency_inherited
+    if ['physical_representation', 'digital_representation','archival_object'].include?(doc['primary_type'])
+      if !!json.dig('responsible_agency', 'inherited')
+        'Y'
+      else
+        'N'
+      end
+    end
+  end
+
+  def repository
+    if doc['primary_type'] == 'location'
+      json['building']
+    end
+  end
+
+  def floor
+    if doc['primary_type'] == 'location'
+      json['floor']
+    end
+  end
+
+  def room
+    if doc['primary_type'] == 'location'
+      json['room']
+    end
+  end
+
+  def area
+    if doc['primary_type'] == 'location'
+      json['area']
+    end
+  end
+
+  def location_profile
+    if doc['primary_type'] == 'location'
+      json.dig('location_profile', '_resolved', 'display_string')
+    end
+  end
+
+  def treatment_status
+    if doc['primary_type'] == 'physical_representation'
+      Array(json['conservation_treatments']).map{|ct| ct['status']}.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.map{|label, count| '%d %s' % [count, label]}.join('; ')
+    end
+  end
+
+  def treatments_applied
+    if doc['primary_type'] == 'physical_representation'
+      Array(json['conservation_treatments']).map{|ct| Array(ct['treatments']).map{|ta| ta['label']}}.flatten.compact.uniq.sort.join('; ')
+    end
+  end
+
+  def date_commenced
+    if doc['primary_type'] == 'physical_representation'
+      Array(json['conservation_treatments']).map{|ct| ct['start_date']}.compact.uniq.sort.join('; ')
+    end
+  end
+
+  def date_completed
+    if doc['primary_type'] == 'physical_representation'
+      Array(json['conservation_treatments']).map{|ct| ct['end_date']}.compact.uniq.sort.join('; ')
+    end
+  end
+
+  def assessment_id
+    if doc['primary_type'] == 'physical_representation'
+      Array(json['conservation_treatments']).map{|ct| ct.dig('assessment', 'ref')}.compact.map{|uri| QSAId.prefixed_id_for(Assessment, JSONModel::JSONModel(:assessment).id_for(uri))}.join('; ')
+    end
+  end
+
+  def created_date
+    if json['create_time']
+      Time.parse(json['create_time']).getlocal
+    end
+  end
+
+  def created_by
+    json['created_by']
+  end
+
+  def last_modified_date
+    if json['user_mtime']
+      Time.parse(json['user_mtime']).getlocal
+    end
+  end
+
+  def last_modified_by
+    json['last_modified_by']
+  end
+end
