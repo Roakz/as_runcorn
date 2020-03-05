ed54b9ef6d66948e4647a8ad6c7fb413ca50a404
Mark Triggs <mark@dishevelled.net>
2020-03-05 13:23:14 +1100

    Don't blow up if we already have this
    The migration DB does!

--- migrations/110_top_container_remarks.rb
+++ migrations/110_top_container_remarks.rb
@@ -3,8 +3,10 @@ require 'db/migrations/utils'
 Sequel.migration do
 
   up do
-    alter_table(:top_container) do
-      add_column(:remarks, :text, :null => true)
+    unless self[:top_container].columns.include?(:remarks)
+      alter_table(:top_container) do
+        add_column(:remarks, :text, :null => true)
+      end
     end
   end
 end
