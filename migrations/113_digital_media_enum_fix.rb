ef6fed291624c2ea9aadc22bba027f0a8d5da936
Mark Triggs <mark@dishevelled.net>
2020-03-19 14:00:33 +1100

    Remove duplicated "contained within" value
    We had two variations with different case.  Confirmed that only the
    lowercase variant is needed.

---
+++ migrations/113_digital_media_enum_fix.rb
@@ -0,0 +1,14 @@
+require 'db/migrations/utils'
+
+Sequel.migration do
+  up do
+    self.transaction do
+      old_enum_id = self[:enumeration_value][:value => 'Digital Media - Other'][:id]
+      new_enum_id = self[:enumeration_value][:value => 'Digital media - Other'][:id]
+
+      self[:digital_representation].filter(:contained_within_id => old_enum_id).update(:contained_within_id => new_enum_id)
+      self[:enumeration_value].filter(:id => old_enum_id).delete
+    end
+  end
+end
+
